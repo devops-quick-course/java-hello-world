5dde0b22dd0b47a87f0dcbdd26e2d8cdc890c820
NavalTest <36785656+NavalTest@users.noreply.github.com>
2020-01-16 11:46:23 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,12 @@
 # t2.micro node with an AWS Tag naming it "HelloWorld"
 
 provider "aws" {
- environment { 
-                AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY')
-                AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
-            }
- # region     = "ap-south-1"
+  access_key =  "${env.AWS_ACCESS_KEY}"
+  secreat_key =  "${env.AWS_SECRET_ACCESS_KEY}" 
+  region     = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "quick_course" {
   ami           = "ami-02913db388613c3e1"
   instance_type = "t2.micro"
 }
