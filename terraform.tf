cea425bc21758f8b70c436de9f8094b8eeffa54a
NavalTest <36785656+NavalTest@users.noreply.github.com>
2020-01-28 22:10:02 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -8,6 +8,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "quick_course" {
-  ami           = "ami-02913db388613c3e1"
+ # ami           = "ami-02913db388613c3e1"
+  ami             = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 }
