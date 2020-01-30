a1da428e5668c27cbff2c2c16bf165cdffdb5f82
NavalTest <36785656+NavalTest@users.noreply.github.com>
2020-01-30 15:18:42 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,4 +11,30 @@ resource "aws_instance" "quick_course" {
  # ami           = "ami-02913db388613c3e1"
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+  
+  security_groups = ["${aws_security_group.docker.name}"]
+  key_name = "${aws_key_pair.deployer.key_name}"
+  connection {
+    user = "ubuntu"
+    key_file = "ssh/key"
+  }
+  provisioner "file" {
+    source = "key.pem"
+    destination = "/home/ubuntu/key.pem"
+  } 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+    #  "sudo apt-get install apt-transport-https ca-certificates",
+    #  "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D",
+    #  "sudo sh -c 'echo \"deb https://apt.dockerproject.org/repo ubuntu-trusty main\" > /etc/apt/sources.list.d/docker.list'",
+      "sudo apt-get remove docker docker-engine docker.io",
+      "sudo apt install docker.io",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+     # "sudo apt-get install -y docker-engine=1.12.0-0~trusty",
+      "sudo apt-get install awscli",
+      "sudo chmod 400 /home/ubuntu/test.pem",
+    ]
+  }
 }
